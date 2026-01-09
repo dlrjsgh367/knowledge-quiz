dec27756083e5dca9e8a9d9d3655829fc37598ad
이건호 <dlrjsgh367@gmail.com>
2026-01-09 16:50:41 +0900

    Feat: vscode extension 추가

---
+++ backend/quiz/src/main/java/com/geonho/quiz/domain/member/entity/Member.java
@@ -0,0 +1,32 @@
+package com.geonho.quiz.domain.member.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@Table(name = "member")
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String email;
+    private String password;
+    private String username;
+    private LocalDateTime created_at;
+    private LocalDateTime updated_at;
+}
